41b5f6f44ec9407bb464edacf7466f6ed08bf63a
Mikheev <ng.mikheev@ya.ru>
2020-11-17 23:10:05 +0300

    Bat scripts updated. Now they can handle paths with spaces.

--- MakeNewLLProject.bat
+++ MakeNewLLProject.bat
@@ -4,7 +4,7 @@
 @xcopy Templates\LLProjectTemplate\* Projects\%NewProjectName%\* /s /e
 
 @echo Set correct names.
-@for /R "Projects\%NewProjectName%" %%x in (LLProjectTemplate.*) do @ren %%x %NewProjectName%.*
+@for /R "Projects\%NewProjectName%" %%x in (LLProjectTemplate.*) do @ren "%%x" %NewProjectName%.*
 
 @echo Project %NewProjectName% created in Projects directory.
 @echo Add this project to existing IAR workspace manually.
